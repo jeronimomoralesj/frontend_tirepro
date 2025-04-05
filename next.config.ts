aff55aad8fa6f0971396884c6a71857d6b338e88
Jeronimo <jeronimo@Jeronimos-MacBook-Pro.local>
2025-04-05 13:21:21 -0500

    changes in next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains: ["tirepro.com.co", "tireproimages.s3.us-east-1.amazonaws.com"]
-  },
   typescript: {
     ignoreBuildErrors: true, 
+  },
+  images:{
+    domains: ["tirepro.com.co", "tireproimages.s3.us-east-1.amazonaws.com"]
   },
 };
 
